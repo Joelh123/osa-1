fe8a140cd70fab3380bb070047ff0e1cfc094fb7
Joel H. <143587522+Joelh123@users.noreply.github.com>
2024-11-19 09:25:04 +0200

    1.9

---
+++ 1.9/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Statistics = (stats) => {
+  if (stats.total === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  else {
+    return (
+      <div>
+        good {stats.good} <br/>
+        neutral {stats.neutral} <br/>
+        bad {stats.bad} <br/>
+        total {stats.total} <br/>
+        average {(stats.good - stats.bad) / stats.total} <br/>
+        positive {stats.good / stats.total * 100} %
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleClickGood} text="good" />
+      <Button handleClick={handleClickNeutral} text="neutral" />      
+      <Button handleClick={handleClickBad} text="bad" />
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} />
+    </div>
+  )
+}
+
+export default App

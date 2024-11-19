feb8cf450491a161a2fca6f342b38fde474e6d33
Joel H. <143587522+Joelh123@users.noreply.github.com>
2024-11-19 08:53:51 +0200

    1.6 done

--- 1.6/src/App.jsx
+++ 1.6/src/App.jsx
@@ -1,34 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Content = (stats) => {
+  return (
+    <div>
+      good {stats.good} <br/>
+      neutral {stats.neutral} <br/>
+      bad {stats.bad}
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleClickGood} text="good" />
+      <Button handleClick={handleClickNeutral} text="neutral" />      
+      <Button handleClick={handleClickBad} text="bad" />
+      <h1>Statistics</h1>
+      <Content good={good} neutral={neutral} bad={bad} />
+    </div>
   )
 }
 
